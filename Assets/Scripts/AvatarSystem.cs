d5cddd17d9d7979db23893715d1408b80872c26e
liron.li <liron.li@outlook.com>
2018-03-25 16:08:20 +0800

    换装完成

--- Assets/Scripts/AvatarSystem.cs
+++ Assets/Scripts/AvatarSystem.cs
@@ -4,64 +4,113 @@ using UnityEngine;
 
 public class AvatarSystem : MonoBehaviour {
 
-    private GameObject girlSource; // 资源 model
-    private GameObject girlTarget; // 骨架对象
-    private Transform girlSourceTrans;
+    public static AvatarSystem _instance;
 
+    // 原始的模型transform
+    private Transform originGirlSourceTrans;
+    // 新模型对象
+    private GameObject girlTarget;
+    // 小女孩所有的资源信息   //部位的名字，部位编号，部位对应的skm
     private Dictionary<string, Dictionary<string, SkinnedMeshRenderer>> girlData = new Dictionary<string, Dictionary<string, SkinnedMeshRenderer>>();
+    // 部位的名字，部位对应的skm
+    private Dictionary<string, SkinnedMeshRenderer> girlSmr = new Dictionary<string, SkinnedMeshRenderer>();// 换装骨骼身上的skm信息
+    //部位的名字，部位对应的skm
+    private string[,] girlStr = new string[,] { { "eyes", "1" }, { "hair", "1" }, { "top", "1" }, { "pants", "1" }, { "shoes", "1" }, { "face", "1" } };
 
-    Transform[] girlHips;
-    private Dictionary<string, SkinnedMeshRenderer> girlsmr = new Dictionary<string, SkinnedMeshRenderer>();
+    Transform[] girlHips; //小女孩骨骼信息
+
+    void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(this); //不删除游戏物体
+    }
 
     private void Start()
     {
-        this.InitSource();
-        this.InitTarget();
-
-        girlHips = girlTarget.GetComponentsInChildren<Transform>();
-        this.SavaData();
+        this.InstantiateGirl();
+        SaveData(originGirlSourceTrans, girlData, girlTarget, girlSmr);
+        InitAvatarGirl();
     }
 
     void InitSource()
     {
-        girlSource = Instantiate(Resources.Load("FemaleModel")) as GameObject;
-        girlSourceTrans = girlSource.transform;
-        //girlSource.SetActive(false);
+
     }
 
-    void InitTarget()
+    void SaveData(Transform souceTrans, Dictionary<string, Dictionary<string, SkinnedMeshRenderer>> data,
+        GameObject target, Dictionary<string, SkinnedMeshRenderer> smr)
     {
-        girlTarget = Instantiate(Resources.Load("FemaleTarget")) as GameObject;
-    }
 
-    void SavaData()
-    {
-        if (girlSourceTrans == null)
-            return;
+        data.Clear();
+        smr.Clear();
 
-        SkinnedMeshRenderer[] parts = girlSourceTrans.GetComponentsInChildren<SkinnedMeshRenderer>();
-        foreach (var part in parts)
+        SkinnedMeshRenderer[] parts = souceTrans.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach(var part in parts)
         {
             string[] names = part.name.Split('-');
-            Debug.Log(names);
-            if (!girlData.ContainsKey(names[0]))
+            if (!data.ContainsKey(names[0]))
             {
                 GameObject partGo = new GameObject();
                 partGo.name = names[0];
-                partGo.transform.parent = girlTarget.transform;
+                partGo.transform.parent = target.transform;
 
-                girlsmr.Add(names[0], partGo.AddComponent<SkinnedMeshRenderer>());
+                smr.Add(names[0], partGo.AddComponent<SkinnedMeshRenderer>());
+                data.Add(names[0], new Dictionary<string, SkinnedMeshRenderer>());
             }
+            data[names[0]].Add(names[1], part);
         }
     }
 
-    void ChangeMash(string part, string num)
-    {
+    void ChangeMesh(string part, string num, Dictionary<string, Dictionary<string, SkinnedMeshRenderer>> data,
+        Transform[] hips, Dictionary<string, SkinnedMeshRenderer> smr, string[,] str)
+    { //传入部位，编号，从data里边拿取对应的skm 
 
+        SkinnedMeshRenderer skm = data[part][num];//要更换的部位
+
+        List<Transform> bones = new List<Transform>();
+        foreach (var trans in skm.bones)
+        {
+            foreach (var bone in hips)
+            {
+                if (bone.name == trans.name)
+                {
+                    bones.Add(bone);
+                    break;
+                }
+            }
+        }
+        //换装实现
+        smr[part].bones = bones.ToArray();//绑定骨骼
+        smr[part].materials = skm.materials;//替换材质
+        smr[part].sharedMesh = skm.sharedMesh;//更换mesh
     }
 
-    void InitAvatar()
+    void InstantiateGirl()
     {
+        // 实例化初始任务模型
+        GameObject go = Instantiate(Resources.Load("FemaleModel")) as GameObject;
+        // 保存原始模型transform
+        this.originGirlSourceTrans = go.transform;
+        // 禁用模型
+        go.SetActive(false);
+        girlTarget = Instantiate(Resources.Load("FemaleTarget")) as GameObject;
+        girlHips = girlTarget.GetComponentsInChildren<Transform>();
+    }
 
+    void InitAvatarGirl()
+    {
+        // 获得行数
+        int length = this.girlStr.GetLength(0);
+        for (int i = 0; i < length; i++)
+        {
+            ChangeMesh(girlStr[i, 0], girlStr[i, 1], girlData, girlHips, girlSmr, girlStr); //穿上衣服
+        }
+    }
+
+    public void OnChangePeople(string part, string num)
+    {
+        Debug.Log(part);
+        Debug.Log(num);
+        this.ChangeMesh(part, num, girlData, girlHips, girlSmr, girlStr);
     }
 }
---
+++ Assets/Scripts/AvatarToggle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarToggle : MonoBehaviour {
+
+
+    public void OnValueChange(bool isOn)
+    {
+        if (isOn)
+        {
+            string[] name = gameObject.name.Split('-');
+            AvatarSystem._instance.OnChangePeople(name[0], name[1]);
+        }
+    }
+}
